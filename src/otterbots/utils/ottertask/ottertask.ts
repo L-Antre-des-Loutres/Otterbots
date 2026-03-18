ac69bef13ebfe4acf4b8fc01cf757581c22d34b4
matheo-1712 <perod.matheo@gmail.com>
2026-03-18 11:00:15 +0100

    Refactorisation de la classe `OtterTask` : simplification des méthodes, ajout de commentaires explicatifs et suppression des lignes de code redondantes.

--- src/otterbots/utils/ottertask/ottertask.ts
+++ src/otterbots/utils/ottertask/ottertask.ts
@@ -1,8 +1,12 @@
+/**
+ * Represents a scheduled task with a name, description, status, and a cron expression.
+ */
 export class OtterTask {
 
     // List of tasks
     private static taskList: OtterTask[] = []
 
+    // Task properties
     private name : string
     private description : string
     private important : boolean
@@ -13,57 +17,29 @@ export class OtterTask {
         this.description = description
         this.important = status
         this.cron_expression = cron_expression
-
     }
 
-    static getTaskList() {
-        return this.taskList
-    }
-
+    /** ****************
+     * TASK LIST METHODS
+     *  ***************** **/
+    static getTaskList() {return this.taskList}
     static getTaskByName(name: string) {
         return this.taskList.find(task => task.name === name)
     }
-
-    static getTaskNumber() {
-        return this.taskList.length
-    }
-
-    static addTask(task: OtterTask) {
-        this.taskList.push(task)
-    }
+    static getTaskNumber() {return this.taskList.length}
+    static addTask(task: OtterTask) {this.taskList.push(task)}
 
     /** ****************
      * GETTERS / SETTERS
      *  ***************** **/
-    getName() {
-        return this.name
-    }
+    getName() {return this.name}
+    getDescription() {return this.description}
+    getStatus() {return this.important}
+    getCronExpression() {return this.cron_expression}
 
-    getDescription() {
-        return this.description
-    }
-
-    getStatus() {
-        return this.important
-    }
-
-    getCronExpression() {
-        return this.cron_expression
-    }
-
-    setName(name: string) {
-        this.name = name
-    }
-
-    setDescription(description: string) {
-        this.description = description
-    }
-
-    setStatus(status: boolean) {
-        this.important = status
-    }
-    setCronExpression(cron_expression: string) {
-        this.cron_expression = cron_expression
-    }
+    setName(name: string) {this.name = name}
+    setDescription(description: string) {this.description = description}
+    setStatus(status: boolean) {this.important = status}
+    setCronExpression(cron_expression: string) {this.cron_expression = cron_expression}
 
 }
